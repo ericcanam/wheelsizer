65acd3cf1798d5d3d4c242839a89ea40d023a406
Eric Canam <eric.canam@live.ca>
2023-12-17 00:03:53 -0500

    Check-Against QR Codes
    - QR Code is now generated.
    - Fancy rainbow sprimkle background.
    - Information about setup on QR code image.
    - New logo meant for QR code.
    - Updated sitemap.xml

--- wheelsizer/src/QuickCheck.vue
+++ wheelsizer/src/QuickCheck.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { ref } from 'vue';
+    import { ref, nextTick } from 'vue';
 
     import TextBar from './components/TextBar.vue';
     import OptionToggle from './components/OptionToggle.vue';
@@ -15,6 +15,9 @@
     import Checklist from './components/Checklist.vue';
 
     import { getCode, readCode } from './codes.js';
+
+    import QrcodeVue from 'qrcode.vue';
+
 
     const QUERY = new URLSearchParams(window.location.search).get('c');
     const CREATE_MODE = !QUERY;
@@ -54,9 +57,12 @@
         }
     }*/
 
+    // URL / QR code stuff
     const urlcontainer = ref();
     const checklistcontainer = ref();
     const qrcanvas = ref();
+    const qrlogo = ref();
+    const qrprops = ref({value: '', renderAs: 'canvas', level: 'M'});
 
     function formsubmit(e){
         e.preventDefault();
@@ -95,7 +101,7 @@
             urlcontainer.value.innerHTML = url;
             urlcontainer.value.setAttribute("href", url);
 
-            drawCanvas(url);
+            drawCanvas(url, oldfront, qrcanvas.value, qrlogo.value);
         }
         else{
             // clear table
@@ -254,21 +260,133 @@
 	}
 
     // draw the QR code
-    function drawCanvas(url){
-        var c = qrcanvas.value;
-        var ctx = c.getContext("2d");
+    function drawCanvas(url, setup, canvas, logo){
+        // get set up
+        var ctx = canvas.getContext("2d");
 
-        const w = c.width;
-        const h = c.height;
+        const w = canvas.width;
+        const h = canvas.height;
 
+        // draw lines
+        const line_sep = 100;
+        const margin = 50;
+        
+        const background = "#272727";
+        const foreground = "#ffffff";
+        const colour = ['#0077ff', '#08b450', '#df9904', '#d83939'];
+        const colours = colour.length;
+
+        ctx.lineCap = "round";
+        ctx.lineWidth = 3;
+
+        // draws background colour
+        ctx.fillStyle = background;
+        ctx.fillRect(0,0, w,h);
+
+        // creates rainbow array of 45 degree lines
+        for(let c=0; c<colours; c++){
+            ctx.strokeStyle = colour[c];
+            ctx.beginPath();
+            for(let y=(line_sep*(c+0.5)); y<=(h+w-(4*margin)); y+=line_sep*colours){
+
+                // x & y offset for lines that start out of bounds
+                let offset = Math.min(0, h-(2*margin)-y);
+
+                // x & y line component
+                let linecomp = Math.min(w-(2*margin), y);
+
+                ctx.moveTo(margin-offset, margin+y+offset);
+                ctx.lineTo(margin+linecomp, margin+y-linecomp);
+
+                // Draw the Path
+                ctx.stroke();
+            }
+            ctx.closePath();
+        }
+
+        const obj_margin = 50;
+        const obj_pad = 50;
+
+        // clears space for logo
+        const logo_w = w-2*obj_margin-2*margin;
+        const logo_h = (logo_w-(2*obj_pad)) * (logo.naturalHeight/logo.naturalWidth) + (2*obj_pad);
+        ctx.beginPath(); 
+        ctx.roundRect(obj_margin+margin,h-obj_margin-logo_h-margin, logo_w,logo_h, 25);
+        ctx.fill();
+        ctx.closePath();
+
+        // draws logo
+        ctx.drawImage(logo, obj_margin+margin+obj_pad, h-(margin+logo_h),
+                            logo_w-2*obj_pad, logo_h-2*obj_pad);
+
+        // clears square for QR code
+        const qrs = h-logo_h-3*obj_margin-2*margin;
+        ctx.beginPath();
+        ctx.roundRect(obj_margin+margin,obj_margin+margin, qrs,qrs, 25);
+        ctx.fill();
+        ctx.closePath();
+
+        // draw QR code
+        qrprops.value.value = url;
+        console.log(url);
+        qrprops.value.size = qrs;
+        qrprops.value.background = background;
+        qrprops.value.foreground = foreground;
+
+        // shove QR code in there
+        let qrraw = document.getElementById("qrraw");
+        nextTick(function(){ // delayed so that qrcode-vue can update its canvas
+            ctx.drawImage(qrraw, obj_margin+margin+obj_pad, obj_margin+margin+obj_pad,
+                                qrs-(2*obj_pad), qrs-(2*obj_pad));
+        }, qrraw);
+
+        // clears space for setup info
+        ctx.beginPath();
+        let textarea_w = w-qrs-(2*margin)-(3*obj_margin);
+        let textarea_x = (2*obj_margin)+margin+qrs;
+        ctx.roundRect(textarea_x,obj_margin+margin, textarea_w,qrs, 25);
+        ctx.fill();
+        ctx.closePath();
+
+        // writes some setup stuff
+        let fontsize = 50;
+        let lineheight = 1.2;
+        ctx.font = "normal normal 300 "+fontsize+"px 'Fira Sans'";
+        ctx.textAlign = "center";
+        // center line
+        let textarea_c = textarea_x+(textarea_w/2);
+        // set font colour
+        ctx.fillStyle = foreground;
+        const lines = [];
+        if(setup.wheels){
+            lines.push(
+                "Wheels",
+                setup.wheels.diameter + "x" + setup.wheels.width + " ET" + setup.wheels.offset,
+                setup.wheels.holes + "x" + setup.wheels.pcd + " CB" + setup.wheels.bore
+            );
+        }if(setup.wheels && setup.tires){
+            lines.push("");
+        }
+        if(setup.tires){
+            lines.push(
+                "Tires",
+                setup.tires.section + "/" + setup.tires.ratio + "R" + setup.tires.diameter
+            );
+        }
+        // calculate total height of text
+        let text_h = (fontsize*(0.5+lines.length)*lineheight);
+        let text_y = (qrs-text_h)/2;
+
+        for(let l=0; l<lines.length; l++){
+            ctx.fillText(lines[l], textarea_c, margin+obj_margin+obj_pad+(fontsize*(0.5+l)*lineheight)+text_y);
+        }
         
     }
     // clear the QR code
-    function clearCanvas(){
-        var c = qrcanvas.value;
-        var ctx = c.getContext("2d");
+    function clearCanvas(object){
+        var ctx = object.getContext("2d");
 
-        ctx.clearRect(0, 0, c.width, c.height);
+        ctx.clearRect(0, 0, object.width, object.height);
     }
 
 
@@ -381,9 +499,18 @@
                 </p>
 
                 <!-- QR Code -->
-                <h2>QR Code:</h2>
+                <h2>
+                    QR Code:
+                    <div class="headingsub">Save &amp; post this with your for-sale ad!</div>
+                </h2>
                 <p>
-                    <canvas id="link_qr" width="1000" height="1300" style="width: 70%;" ref="qrcanvas" />
+                    <!-- canvas -->
+                    <canvas id="link_qr" width="1300" height="1300" style="max-width:100%;" ref="qrcanvas" />
+
+                    <!-- logo -->
+                    <img ref="qrlogo" src="/assets/logo_u3.png" style="display:none;" />
+                    <!-- raw QR -->
+                    <qrcode-vue id="qrraw" v-bind="qrprops" style="display:none;"></qrcode-vue>
                 </p>
             </div>
             <div class="sidebyside" v-else>
@@ -407,14 +534,14 @@
                     will fit or be a safe choice for your vehicle.
                 </InfoBox>
             </div></div>
-            <div class="row" v-if="!CREATE_MODE"><div class="sidebyside">
+            <!--<div class="row" v-if="!CREATE_MODE"><div class="sidebyside">
                 <h2>Visual Reference</h2>
                 <p>
                     
                 </p>
             </div><div class="sidebyside">
                 
-            </div></div>
+            </div></div>-->
 		</form>
 	</main>
     <footer>
